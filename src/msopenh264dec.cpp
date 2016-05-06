gguoling/msopenh264

commit 3a0f6e84fc3ccec30fb19cc5faaa421cf613b064

update for latest openh264

src/msopenh264dec.cpp
@@ -53,7 +53,6 @@ void MSOpenH264Decoder::initialize()
 		mUnpacker=rfc3984_new();
 		if (mDecoder != 0) {
 			SDecodingParam params = { 0 };
-			params.eOutputColorFormat = videoFormatI420;
 			params.uiTargetDqLayer = (unsigned char) -1;
 			params.eEcActiveIdc = ERROR_CON_FRAME_COPY_CROSS_IDR;
 			params.sVideoProperty.size = sizeof(params.sVideoProperty);

src/msopenh264enc.cpp
@@ -122,14 +122,14 @@ void MSOpenH264Encoder::initialize()
 			params.bEnableLongTermReference  = false;
 			params.iSpatialLayerNum=1;
 			params.eSpsPpsIdStrategy = CONSTANT_ID;
-			
+
 			params.sSpatialLayers[0].iVideoWidth = mVConf.vsize.width;
 			params.sSpatialLayers[0].iVideoHeight = mVConf.vsize.height;
 			params.sSpatialLayers[0].fFrameRate = mVConf.fps;
 			params.sSpatialLayers[0].iSpatialBitrate = targetBitrate;
 			params.sSpatialLayers[0].iMaxSpatialBitrate = maxBitrate;
-			params.sSpatialLayers[0].sSliceCfg.uiSliceMode = SM_DYN_SLICE;
-			params.sSpatialLayers[0].sSliceCfg.sSliceArgument.uiSliceSizeConstraint = ms_factory_get_payload_max_size(mFilter->factory);
+			params.sSpatialLayers[0].sSliceArgument.uiSliceMode = SM_SIZELIMITED_SLICE;
+			params.sSpatialLayers[0].sSliceArgument.uiSliceSizeConstraint = ms_factory_get_payload_max_size(mFilter->factory);
 
 			ret = mEncoder->InitializeExt(&params);
 			if (ret != 0) {

commit 7744dc572470de6caf11b7e4ce7e79ba0c5efd55

Fix compilation with latest openh264 release that had been broken with 3a0f6e84fc3ccec30fb19cc5faaa421cf613b064.

src/msopenh264dec.cpp
@@ -53,6 +53,10 @@ void MSOpenH264Decoder::initialize()
 		mUnpacker=rfc3984_new();
 		if (mDecoder != 0) {
 			SDecodingParam params = { 0 };
+#if (OPENH264_MAJOR == 1) && (OPENH264_MINOR >=6)
+#else
+			params.eOutputColorFormat = videoFormatI420;
+#endif
 			params.uiTargetDqLayer = (unsigned char) -1;
 			params.eEcActiveIdc = ERROR_CON_FRAME_COPY_CROSS_IDR;
 			params.sVideoProperty.size = sizeof(params.sVideoProperty);

src/msopenh264enc.cpp
@@ -126,8 +126,13 @@ void MSOpenH264Encoder::initialize()
 			params.sSpatialLayers[0].fFrameRate = mVConf.fps;
 			params.sSpatialLayers[0].iSpatialBitrate = targetBitrate;
 			params.sSpatialLayers[0].iMaxSpatialBitrate = maxBitrate;
+#if (OPENH264_MAJOR == 1) && (OPENH264_MINOR >=6)
 			params.sSpatialLayers[0].sSliceArgument.uiSliceMode = SM_SIZELIMITED_SLICE;
 			params.sSpatialLayers[0].sSliceArgument.uiSliceSizeConstraint = ms_factory_get_payload_max_size(mFilter->factory);
+#else
+			params.sSpatialLayers[0].sSliceCfg.uiSliceMode = SM_DYN_SLICE;
+			params.sSpatialLayers[0].sSliceCfg.sSliceArgument.uiSliceSizeConstraint = ms_factory_get_payload_max_size(mFilter->factory);
+#endif
 
 			ret = mEncoder->InitializeExt(&params);
 			if (ret != 0) {